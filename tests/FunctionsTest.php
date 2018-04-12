1038d2ade57ce3aa53371f256d053fa3100c17b7
Anton Shabovta <sh@onliner.by>
2018-04-12 10:38:06 +0300

    Functions tests added

---
+++ tests/FunctionsTest.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * This file is part of PHPinnacle/Ensign.
+ *
+ * (c) PHPinnacle Team <dev@phpinnacle.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PHPinnacle\Ensign\Tests;
+
+class FunctionsTest extends EnsignTest
+{
+    /**
+     * Test that Dispatcher can dispatch proper signals
+     *
+     * @test
+     */
+    public function dispatchSignal()
+    {
+        self::loop(function () {
+            ensign_signal('upper', function ($text) {
+                return strtoupper($text);
+            });
+
+            self::assertTask($upperTask = ensign_dispatch('upper', 'test'));
+            self::assertEquals('TEST', yield $upperTask);
+        });
+    }
+
+    /**
+     * Test acquire signals list
+     *
+     * @test
+     */
+    public function pcntlSignalList()
+    {
+        $exclude = ['SIGHUP', 'SIGINT'];
+        $signals = pcntl_signal_list();
+
+        $this->assertEquals($exclude, \array_diff($signals, pcntl_signal_list($exclude)));
+    }
+}
